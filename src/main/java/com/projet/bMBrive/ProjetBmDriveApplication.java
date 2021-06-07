43ee8e71a3e4b396d5f82a2909291560ab487dd9
Alexis <alexis19971712@gmail.com>
2021-06-07 15:40:46 +0200

    first commit

---
+++ src/main/java/com/projet/bMBrive/ProjetBmDriveApplication.java
@@ -0,0 +1,13 @@
+package com.projet.bMBrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetBmDriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetBmDriveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projet/bMBrive/ProjetBmDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet.bMBrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetBmDriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
